e1955bd4011beba2e0200e6689af22a7b1b840c8
MichaelReece <michael.reece@live.com.au>
2022-11-25 15:47:02 +1100

    began creating game class

---
+++ headers/game.hh
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "entity.hh"
+
+#include "room.hh"
+#include "gameState.hh"
+
+#ifndef GAME_HH
+#define GAME_HH
+
+class Game {
+private:
+
+public:
+    GameState start();
+    GameState load();
+};
+
+#endif
