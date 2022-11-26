334e4919030d744521ecc1c47c7e4a1fde968b58
MichaelReece <michael.reece@live.com.au>
2022-11-26 11:41:59 +1100

    Began creating player class

---
+++ headers/player.hh
@@ -0,0 +1,16 @@
+
+
+#include "character.hh"
+#include "equipment.hh"
+
+#ifndef PLAYER_HH
+#define PLAYER_HH
+
+class Player : public Character
+{
+private:
+    std::vector<Equipment> equipment;
+
+};
+
+#endif
