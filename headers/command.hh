c122ce13750f8e8a4a6718008da69bbd6f319210
MichaelReece <michael.reece@live.com.au>
2022-11-26 13:23:18 +1100

    Fleshed out command & commandHandler

--- headers/command.hh
+++ headers/command.hh
@@ -1,18 +1,40 @@
 #include <string>
 #include <vector>
 
-
+#include "entity.hh"
 
 #ifndef COMMAND_HH
 #define COMMAND_HH
 
-template <typename T>
+using f_ptr = void (*)(std::string, std::vector<Entity*>);
 
 class Command
 {
 private:
     std::vector<std::string> aliases;
-    void (*method)(std::string, std::vector<T>);
+    f_ptr method;
+
+public:
+    //getters and setters
+    std::vector<std::string> getAliases() { return aliases; }
+    f_ptr                    getMethod() { return method; }
+    void                     setAliases(const std::vector<std::string>& aliases) { this->aliases = aliases; }
+    void                     setMethod(const f_ptr& method) { this->method = method; }
+
+    //constructors
+    Command() = delete;
+    Command(const std::vector<std::string>& aliases, const f_ptr method)
+    {
+        this->aliases = aliases;
+        this->method = method;
+    }
+
+    //methods
+    void execute(const std::string& arg1, const std::vector<Entity*>& arg2)
+    {
+        method(arg1, arg2);
+    }    
+
 };
 
 #endif
--- headers/commandHandler.hh
+++ headers/commandHandler.hh
@@ -1,13 +1,32 @@
-
+#include <string>
+#include <vector>
 
 #include "command.hh"
+#include "player.hh"
+#include "room.hh"
+#include "game.hh"
 
 #ifndef COMMANDHANDLER_HH
 #define COMMANDHANDLER_HH
 
 class CommandHandler
 {
+private:
+    Game* game;
+    std::vector<Command> commandList;
 
+public:
+    //constructors
+    CommandHandler(Game* game, std::vector<Command> commandList)
+    {
+        this->game = game;
+        this->commandList = commandList;
+    }
+
+    void addCommands(const Command& command);
+    void addCommands(const std::vector<Command>& commandList);
+
+    void handle(Player& player, Room& currentRoom, std::string cmnd);
 };
 
 #endif
